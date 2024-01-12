ec9dbf3b42a14cf31cd1ac4f4faf58b1f2f3298f
hinkers <24424803+hinkers@users.noreply.github.com>
2024-01-12 15:25:14 +1100

    Some nice visual improvements

--- lua/neotasks.lua
+++ lua/neotasks.lua
@@ -15,11 +15,13 @@ function M.open_todo_list()
     vim.cmd("vertical resize 60")
     vim.cmd("wincmd h")
     if vim.fn.filereadable(todo_file_path) == 0 then
-        vim.fn.writefile({}, todo_file_path)
+        vim.fn.writefile({"# Todo List", "", ""}, todo_file_path)
     end
     api.nvim_command('edit ' .. todo_file_path)
 
     local bufnr = api.nvim_get_current_buf()
+
+    api.nvim_command('setlocal filetype=todolist')
 
     -- Set buffer-local keymaps
     api.nvim_buf_set_keymap(bufnr, 'n', '<leader>td', '<cmd>lua require("neotasks").add_todo_item()<CR>', {noremap = true, silent = true, desc = "Add new todo item"})
@@ -34,9 +36,14 @@ end
 
 -- Function to add new Todo item
 function M.add_todo_item()
-    local current_line_count = api.nvim_buf_line_count(0)
-    api.nvim_buf_set_lines(0, current_line_count, current_line_count, false, {new_item_text})
-    api.nvim_win_set_cursor(0, {current_line_count + 1, #new_item_text})
+    local row, col = unpack(api.nvim_win_get_cursor(0))
+    -- Insert new line with "[ ] " right below the current line
+    api.nvim_buf_set_lines(0, row, row, false, {new_item_text, ""})
+    
+    -- Move cursor to the beginning of the new line
+    api.nvim_win_set_cursor(0, {row + 1, #new_item_text - 1})
+
+    -- Enter insert mode
     api.nvim_command("startinsert!")
     save_todo_list()
 end
@@ -56,20 +63,22 @@ end
 function M.archive_todo_item()
     local row, col = unpack(api.nvim_win_get_cursor(0))
     local line = api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-    local date = os.date("%Y-%m-%d")
-    local archive_file_path = archive_base_path .. "archive_" .. date .. ".txt"
-    
-    -- Check if the archive file exists and create it if it doesn't
-    if vim.fn.filereadable(archive_file_path) == 0 then
-        vim.fn.writefile({}, archive_file_path)
+    local total_lines = api.nvim_buf_line_count(0)
+    local last_line = api.nvim_buf_get_lines(0, total_lines - 1, total_lines, false)[1]
+
+    -- Check if the last line is a completed item, if not add a newline
+    if not last_line:find("^" .. vim.pesc(complete_item_text)) then
+        api.nvim_buf_set_lines(0, total_lines, total_lines, false, {""})
     end
 
-    local archive_content = vim.fn.readfile(archive_file_path)
-    table.insert(archive_content, line)
-    vim.fn.writefile(archive_content, archive_file_path)
-    
-    -- Delete the current line
-    api.nvim_buf_set_lines(0, row - 1, row, false, {})
+    -- Move the completed item to the bottom of the file
+    api.nvim_buf_set_lines(0, total_lines, total_lines, false, {line})
+
+    -- Delete the original line of the completed item
+    if row ~= total_lines then  -- Only delete the original line if it's not the last line
+        api.nvim_buf_set_lines(0, row - 1, row, false, {})
+    end
+
     save_todo_list()
 end
 
@@ -106,6 +115,36 @@ local function init()
         vim.fn.mkdir(archive_base_path, "p")
     end
 
+    -- Create syntax highlighting
+    local complete_item_pattern = vim.pesc(complete_item_text)
+    local new_item_pattern = vim.pesc(new_item_text)
+
+    vim.cmd("syntax match TodoNew '^" .. vim.pesc(new_item_text) .. ".*$'")
+    vim.cmd("syntax match TodoComplete '^" .. vim.pesc(complete_item_text) .. ".*$'")
+    vim.cmd([[
+        syntax match TodoHeader /^#.*$/
+        syntax match TodoSubHeader /^##.*$/
+        highlight link TodoNew Normal
+        highlight link TodoComplete Comment
+        highlight link TodoHeader Statement
+        highlight link TodoSubHeader Type
+    ]])
+
+
+    vim.cmd([[
+        augroup TodoListSyntax
+        autocmd!
+        autocmd FileType todolist syntax match TodoNew '^]] .. new_item_pattern .. [[.*$'
+        autocmd FileType todolist syntax match TodoComplete '^]] .. complete_item_pattern .. [[.*$'
+        autocmd FileType todolist syntax match TodoHeader /^#.*$/
+        autocmd FileType todolist syntax match TodoSubHeader /^##.*$/
+        autocmd FileType todolist highlight link TodoNew Normal
+        autocmd FileType todolist highlight link TodoComplete Comment
+        autocmd FileType todolist highlight link TodoHeader Statement
+        autocmd FileType todolist highlight link TodoSubHeader Type
+        augroup END
+    ]])
+    
     -- Register commands and keybindings
     api.nvim_create_user_command('TodoList', M.open_todo_list, {})
     api.nvim_create_user_command('TodoArchives', M.open_archive_selector, {})
