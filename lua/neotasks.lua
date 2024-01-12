38959094d01c25fd8dd1dddc7c59260807d8b20e
nhinkley-mtdps <24424803+hinkers@users.noreply.github.com>
2024-01-12 12:09:24 +1100

    Adding archive view list

--- lua/neotasks.lua
+++ lua/neotasks.lua
@@ -1,6 +1,9 @@
 local api = vim.api
 local M = {}
 
+local new_item_text = "[ ] "
+local complete_item_text = "[x] "
+
 local todo_file_name = "todo.txt"
 local todo_base_path = vim.fn.expand("~/NeoTasks/")
 local todo_file_path = todo_base_path .. todo_file_name
@@ -32,11 +35,8 @@ end
 -- Function to add new Todo item
 function M.add_todo_item()
     local current_line_count = api.nvim_buf_line_count(0)
-    -- Add new line with "[ ] " at the end of the buffer
-    api.nvim_buf_set_lines(0, current_line_count, current_line_count, false, {"[ ] "})
-    -- Place cursor at the end of the new line
-    api.nvim_win_set_cursor(0, {current_line_count + 1, 4})
-    -- Switch to insert mode
+    api.nvim_buf_set_lines(0, current_line_count, current_line_count, false, {new_item_text})
+    api.nvim_win_set_cursor(0, {current_line_count + 1, #new_item_text})
     api.nvim_command("startinsert")
     save_todo_list()
 end
@@ -45,8 +45,10 @@ end
 function M.complete_todo_item()
     local row, col = unpack(api.nvim_win_get_cursor(0))
     local line = api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-    -- Mark the current line as complete
-    api.nvim_buf_set_lines(0, row - 1, row, false, {"[x] " .. line})
+    if line:sub(1, #new_item_text) == new_item_text then
+        line = complete_item_text .. line:sub(#new_item_text + 1)
+    end
+    api.nvim_buf_set_lines(0, row - 1, row, false, {line})
     save_todo_list()
 end
 
@@ -71,18 +73,30 @@ function M.archive_todo_item()
     save_todo_list()
 end
 
+-- Function to list and select archive files
+function M.open_archive_selector()
+    local archives = vim.fn.globpath(archive_base_path, "*.txt", false, true)
+    local options = { relative = 'editor', width = 60, height = 20, row = 10, col = 10 }
+    local bufnr = api.nvim_create_buf(false, true)
+    api.nvim_open_win(bufnr, true, options)
+    api.nvim_buf_set_lines(bufnr, 0, -1, false, archives)
+    -- TODO: Add more logic here to make these files selectable
+end
+
 -- Initialization function to create necessary directories
 local function init()
     if vim.fn.isdirectory(todo_base_path) == 0 then
         vim.fn.mkdir(todo_base_path, "p")  -- 'p' flag to create parent directories as needed
         vim.fn.mkdir(archive_base_path, "p")
     end
+
+    -- Register commands and keybindings
+    api.nvim_create_user_command('TodoList', M.open_todo_list, {})
+    api.nvim_create_user_command('OpenArchives', M.open_archive_selector, {})
 end
 
 -- Run the initalization function
 init()
 
--- Register commands and keybindings
-api.nvim_create_user_command('TodoList', M.open_todo_list, {})
 
 return M
