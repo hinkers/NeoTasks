240da2fcd297222b264657cce543690cc6391528
nhinkley-mtdps <24424803+hinkers@users.noreply.github.com>
2024-01-12 11:41:14 +1100

    Promote functions from local

--- lua/neotasks.lua
+++ lua/neotasks.lua
@@ -5,7 +5,7 @@ local todo_file_path = vim.fn.expand("~/NeoTasks/todo.txt")
 local archive_base_path = vim.fn.expand("~/NeoTasks/archives")
 
 -- Function to open Todo list pane
-local function M.open_todo_list()
+function M.open_todo_list()
     -- Split window and open or create todo list file
     vim.cmd("vsplit")
     vim.cmd("wincmd h")
@@ -22,7 +22,7 @@ local function save_todo_list()
 end
 
 -- Function to add new Todo item
-local function M.add_todo_item()
+function M.add_todo_item()
     -- Add new line at the end of the buffer
     api.nvim_buf_set_lines(0, -1, -1, false, {""})
     -- Go to the new line
@@ -33,7 +33,7 @@ local function M.add_todo_item()
 end
 
 -- Function to mark Todo item as complete
-local function M.complete_todo_item()
+function M.complete_todo_item()
     local row, col = unpack(api.nvim_win_get_cursor(0))
     local line = api.nvim_buf_get_lines(0, row - 1, row, false)[1]
     -- Mark the current line as complete
@@ -42,7 +42,7 @@ local function M.complete_todo_item()
 end
 
 -- Function to archive Todo item
-local function M.archive_todo_item()
+function M.archive_todo_item()
     local row, col = unpack(api.nvim_win_get_cursor(0))
     local line = api.nvim_buf_get_lines(0, row - 1, row, false)[1]
     local date = os.date("%Y-%m-%d")
